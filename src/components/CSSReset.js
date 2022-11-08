9d36f6915c5c133ae7147aa5861c1435b7ee1fe9
j-novaes <joaovictor.nb21@hotmail.com>
2022-11-08 12:07:26 +0000

    Define project font-family

--- src/components/CSSReset.js
+++ src/components/CSSReset.js
@@ -6,6 +6,7 @@ export const CSSReset = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: Helvetica, Arial;
   }
   body {
     font-family: sans-serif;
